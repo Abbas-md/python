2329152dd9757f21a9d8a5476eb6543407df8350
Abbas-md <35156834+Abbas-md@users.noreply.github.com>
2026-04-23 13:32:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,5 +6,6 @@ resource "aws_instance" "my-instance" {
   ami           = "ami-0e12ffc2dd465f6e4"
   instance_type = "t2.micro"
   subnet_id = "subnet-067fefba38aba289d"
+  key_name = "devops-key"
   security_groups = ["sg-061659dc1a3829f18"]
 }
